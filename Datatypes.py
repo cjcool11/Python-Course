308a3d6afa3bdfbe243dce9d85fa5f91e9400c3c
cjcool11 <aishaabdulkarim009@gmail.com>
2024-08-30 17:17:28 +0400

    class no.3

---
+++ Datatypes.py
@@ -0,0 +1,14 @@
+name = "dude"
+age = 13
+weight = 42.8
+issmart = True
+
+print("name is a", type(name))
+print("age is a", type(age))
+print("weight is a", type(weight))
+print("issmart is a", type(issmart))
+print("datatype after type casting")
+age = float(age)
+print("Now age is a", type(age))
+weight = int(weight)
+print("now weight is a", type(weight))
