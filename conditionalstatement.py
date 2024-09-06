c2804986ed833b3aeae06ad2b16cc41b6bf9302f
cjcool11 <aishaabdulkarim009@gmail.com>
2024-09-06 17:07:05 +0400

    Conditionalstatements

---
+++ conditionalstatement.py
@@ -0,0 +1,32 @@
+A = 2533
+if A < 2534:
+    print("A is less than 2534")
+elif A == 2534:
+    print("A is equal to 2534")
+else:
+    print("A is more than 2534")
+#actual activity 1
+B = int(input("Write a number"))
+if B < 0:
+    print("Your number is a negative number")
+else:
+    print("Your number is a positive number")
+#activity 2
+C = int(input("write how much you spent on something: "))
+D = int(input("Write how much you sold it for: "))
+if C > D:
+    print("You lost some money")
+else:
+    print("you made profit")
+#activity 3
+UserAnswer = int(input("write a number: "))
+if UserAnswer < 15:
+    print("your number is less than 15")
+else:
+    print("your number is more than 15")
+#activity 4
+Input = int(input("write a integer: "))
+if Input % 2 == 0:
+    print("your number is even")
+else:
+    print("your number is odd")
