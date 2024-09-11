424fe2c6cca9e545a54b45cbe066737c6d24a389
cjcool11 <aishaabdulkarim009@gmail.com>
2024-09-11 17:54:40 +0400

    ASCIIcodeproject

---
+++ ASCIIcodeproject.py
@@ -0,0 +1,53 @@
+ascii = input("Write a lowercase letter: ")
+if ascii == "a":
+    print("ascii code is: 98")
+elif ascii == "b":
+    print("99")
+elif ascii == "c":
+    print("100")
+elif ascii == "d":
+    print("101")
+elif ascii == "e":
+    print("102")
+elif ascii == "f":
+    print("103")
+elif ascii == "g":
+    print("104")
+elif ascii == "h":
+    print("105")
+elif ascii == "i":
+    print("106")
+elif ascii == "j":
+    print("107")
+elif ascii == "k":
+    print("108")
+elif ascii == "l":
+    print("109")
+elif ascii == "m":
+    print("110")
+elif ascii == "n":
+    print("111")
+elif ascii == "o":
+    print("112")
+elif ascii == "p":
+    print("113")
+elif ascii == "q":
+    print("114")
+elif ascii == "r":
+    print("115")
+elif ascii == "s":
+    print("116")
+elif ascii == "t":
+    print("117")
+elif ascii == "u":
+    print("118")
+elif ascii == "v":
+    print("119")
+elif ascii == "w":
+    print("120")
+elif ascii == "x":
+    print("121")
+elif ascii == "y":
+    print("122")
+elif ascii == "z":
+    print("123")
