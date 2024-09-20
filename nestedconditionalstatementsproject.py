89cd1bfd782f3dae9a62ad1f40463ce6a96b19b3
cjcool11 <aishaabdulkarim009@gmail.com>
2024-09-20 17:42:16 +0400

    nestedconditionalstatementsproject

---
+++ nestedconditionalstatementsproject.py
@@ -0,0 +1,9 @@
+UserAge = int(input("Type your age: "))
+if UserAge > 10:
+    print("you are older than 10 years old!")
+else:
+    print("you are younger than 10 years old")
+    if UserAge < 20:
+        print("you are under 20")
+    else:
+        print("you are older than 20")
