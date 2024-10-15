52d54a74ecdeadbbfdc5ed525bac640bc39df2eb
cjcool11 <aishaabdulkarim009@gmail.com>
2024-10-15 17:09:02 +0400

    functionsproject

--- functionsproject.py
+++ functionsproject.py
@@ -4,4 +4,4 @@ def circumference(r):
     res = 2*3.14*r
     return res
 
-print("the circumference of your circle is: ",circumference)
+print("the circumference of your circle is: ",circumference(r))
