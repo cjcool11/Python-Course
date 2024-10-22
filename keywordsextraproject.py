dfb8bd526b909d7b5883aa9fa060f7d203b38a60
cjcool11 <aishaabdulkarim009@gmail.com>
2024-10-22 17:35:07 +0400

    ultimate math quiz!!

---
+++ keywordsextraproject.py
@@ -0,0 +1,22 @@
+print("welcome to the ultimate math quiz!!!")
+
+print("if one box has 8 apples and Jack buys 8 boxes, how many apples are there alltogether?")
+
+answer = int(input("answer here: "))
+
+if answer == 64:
+    print("good job!")
+    print("what is 2 * 4")
+    answer = int(input("answer here: "))
+else:
+    pass
+if answer == 8:
+    print("good job again")
+    print("12 * 12 = ?")
+    answer = int(input("answer here: "))
+else:
+    pass
+if answer == 132:
+    print("you are really smart!")
+else:
+    pass
