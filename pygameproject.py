d10c58dcbaf7ff8ce9186d1ba56e5d4498af4b25
cjcool11 <aishaabdulkarim009@gmail.com>
2024-12-24 18:07:08 +0400

    pygameproject

---
+++ pygameproject.py
@@ -0,0 +1,16 @@
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption('Pygame Window')
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    
+    pygame.display.flip()
+
+pygame.quit()
