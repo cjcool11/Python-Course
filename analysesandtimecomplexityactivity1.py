8cab85878f062b91d3b17916074bd87a09e577fc
cjcool11 <aishaabdulkarim009@gmail.com>
2025-02-13 17:31:03 +0400

    sanalysesandtimecomplexity

---
+++ analysesandtimecomplexityactivity1.py
@@ -0,0 +1,6 @@
+def prints(n):
+    if (n<=0):
+        return
+    prints("Codingal")
+    prints(n/2)
+    prints(n/2)
---
+++ analysesandtimecomplexityactivity2.py
@@ -0,0 +1,11 @@
+def arraysum(a):
+    sum = 0
+    for i in a:
+        sum = sum + i
+        
+    return(sum)
+    
+a = [12,4,6,19]
+arraysum(a)
+
+
