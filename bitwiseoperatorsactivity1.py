28547266f342071820d13ab8031908f970643810
cjcool11 <aishaabdulkarim009@gmail.com>
2025-03-04 18:25:19 +0400

    bitwise operators

---
+++ bitwiseoperatorsactivity1.py
@@ -0,0 +1,19 @@
+num1 = 5
+num2 = 3
+
+
+print("num1 & num2 = ",num1 & num2)
+
+print("\nnum1 | num2 = ",num1 | num2)
+
+print("\nnum1 ~ num2 = ",~num1)
+
+print("\nnum1 ^ num2 = ",num1 ^ num2)
+
+print("\nnum1 << 1 = ",num1 << 1)
+
+print("\nnum2 << 1 = ",num2 << 1)
+
+print("\nnum1 >> 1 = ",num1 >> 1)
+
+print("\nnum2 >> 1 = ",num2 >> 1)
