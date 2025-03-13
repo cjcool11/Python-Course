7001d14b6f7897eab5bec15aee7dfe167f102ecc
cjcool11 <aishaabdulkarim009@gmail.com>
2025-03-13 17:51:21 +0400

    bitplay2

--- bitplay2activity3.py
+++ bitplay2activity3.py
@@ -1,32 +1,25 @@
 def twoodd(arr, size):
     xorof2 = arr[0]
-    x= 0
-    y= 0
-    #set bit = 0
+    x = 0
+    y = 0
     for i in range(1, size):
         xorof2 = xorof2 ^ arr[i]
 
     setbit = xorof2 & ~(xorof2 - 1)
 
     for i in range(size):
-        if(arr[i] & setbit):
+        if arr[i] & setbit:
             x = x ^ arr[i]
         else:
             y = y ^ arr[i]
 
-        print("the two ODD elements are: ",x,"&",y,)
-
+    print("the two ODD elements are: ", x, "&", y)
 
 arr = []
 
-arraysize = int(input("enter array size: "))
+arraysize = int(input("Enter array size: "))
 for i in range(0, arraysize):
-    z = int(input("Enter an element"))
+    z = int(input("Enter an element: "))
     arr.append(z)
 
-twoodd(arr,arraysize)
-
-
-
-
-
+twoodd(arr, arraysize)
