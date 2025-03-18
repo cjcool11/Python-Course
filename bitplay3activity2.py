0660d60d85bcbf09b8c77dfaeeb3e7ff2b97adac
cjcool11 <aishaabdulkarim009@gmail.com>
2025-03-18 18:25:47 +0400

    bitplay3

---
+++ bitplay3activity2.py
@@ -0,0 +1,19 @@
+def divide(ousdividend,ourdivisor):
+    sign = (-1 if((ousdividend<0)^(ourdivisor<0))else 1)
+    ousdividend = abs(ousdividend)
+    ourdivisor = abs(ourdivisor)
+
+    quotientnumber = 0
+    tempnumber = 0
+
+    for i in range(31, -1, -1):
+        if (tempnumber + (ourdivisor << i) <= ousdividend):
+            tempnumber += ourdivisor << i
+            quotientnumber |= 1 << i
+        if sign ==-1:
+            quotientnumber -= quotientnumber
+        return quotientnumber
+    
+a = int(input("Enter your dividend: "))
+b = int(input("Enter your divisor: "))
+print("a/b = ",divide(a,b))        
