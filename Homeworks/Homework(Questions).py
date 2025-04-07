659acc23672e359b8f10b9ef6ae5d2d482b675c8
cjcool11 <aishaabdulkarim009@gmail.com>
2025-04-07 18:33:00 +0400

    Homework

---
+++ Homeworks/Homework(Questions).py
@@ -0,0 +1,12 @@
+#Reverse With Recursion Lesson:
+#Question1:
+Name = input("Write your name: ")
+print(Name)
+#Question2:
+Num1 = int(input("Enter a number: "))
+Num2 = int(input("Please enter another number: "))
+print(Num1+Num2)
+#Question3:
+Age = int(input("Enter your age: "))
+Future_Age = Age + 1
+print("You will be: ",Future_Age,"Next Year!!!")
